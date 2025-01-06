80f8211b591a3d311cf85b6c8da6a65f074b0743
bzeizinger <bennet.zeizinger@gmail.com>
2025-01-06 22:03:44 +0100

    Filepicker

--- main.py
+++ main.py
@@ -3,7 +3,30 @@ from tkinter import ttk, filedialog as fd
 
 root = tk.Tk()
 root.title("Manim Output GUI")
-# root.resizable(False, False)
-# root.geometry("")
+
+directory = tk.StringVar(value="No directory selected")
+
+selected_directory = ""
+
+def open_directory():
+    selected_directory = fd.askdirectory()
+    
+    if selected_directory:
+        directory.set(selected_directory)
+        
+    return selected_directory
+
+select_directory_button = ttk.Button(
+    root,
+    text="Select Directory",
+    command=open_directory
+)
+select_directory_button.pack(padx=10, pady=10)
+
+directory_label = ttk.Label(
+    root,
+    textvariable=directory
+)
+directory_label.pack(padx=10, pady=10)
 
 root.mainloop()
