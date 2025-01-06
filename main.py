32a45f0bff8910b86bc47398c9d14839d0b50b3a
bzeizinger <bennet.zeizinger@gmail.com>
2025-01-06 21:42:07 +0100

    initialized tkinter

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import ttk, filedialog as fd
+
+root = tk.Tk()
+root.title("Manim Output GUI")
+# root.resizable(False, False)
+# root.geometry("")
+
+root.mainloop()
